05a4a6f7758242979fb11f058cb5022efbd1698a
gshsong <2460526449@qq.com>
2020-01-30 16:40:54 +0800

    First Commit

---
+++ src/test/java/com/volunteer/uapply/UapplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunteer.uapply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UapplyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
